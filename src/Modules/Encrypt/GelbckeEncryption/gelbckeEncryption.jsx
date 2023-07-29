75588fc5582f7a92b0db741f09f8e4c5df84a5f0
Gelbcke <gabrielgelbcke@gmail.com>
2023-07-29 16:33:52 -0300

    *

--- src/Modules/Encrypt/GelbckeEncryption/gelbckeEncryption.jsx
+++ src/Modules/Encrypt/GelbckeEncryption/gelbckeEncryption.jsx
@@ -15,6 +15,10 @@ const GelbckeEncryption = () => {
     }
   }, []);
 
+  async function waitOneSecondAsync() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
   function convertToEncryption() {
     let text = document.getElementById("input").value;
 
@@ -44,13 +48,75 @@ const GelbckeEncryption = () => {
           new_phrase.push(word_setter);
         }
       }
+      (async () => {
+        await waitOneSecondAsync();
+        document.getElementById("input").value = "TEXT DELETED!";
+      })();
     }
-    console.log(new_phrase);
     new_phrase = new_phrase.join(" ");
     document.getElementById("result").value = new_phrase;
   }
 
-  function convertToText() {}
+  function convertToText() {
+    let text = document.getElementById("result").value;
+
+    let splitted = text.split(" ");
+    let new_phrase = [];
+    let word_setter = "";
+    let char_code = [];
+    let second_letter = "";
+    let last_letter = "";
+
+    if (text == "") {
+      /* document.getElementById("modal").style.display = "inline"; */
+      window.alert("Please insert your text in the encryption field!");
+    } else {
+      for (let i = 0; i < splitted.length; i++) {
+        word_setter = splitted[i].split("");
+
+        word_setter = word_setter.map((item) => {
+          if (!isNaN(item)) {
+            return Number(item);
+          } else {
+            return item;
+          }
+        });
+
+        for (let i = 0; i < word_setter.length; i++) {
+          if (typeof word_setter[i] == "number") {
+            char_code.push(word_setter[i]);
+          }
+        }
+        char_code = char_code.join("");
+
+        if (char_code >= 10 && char_code <= 99) {
+          console.log("2");
+          word_setter = word_setter.slice(2);
+          word_setter.unshift(parseInt(char_code));
+        } else if (char_code >= 100 && char_code <= 999) {
+          word_setter = word_setter.slice(3);
+          word_setter.unshift(parseInt(char_code));
+        }
+        char_code = [];
+
+        word_setter[0] = String.fromCodePoint(word_setter[0]);
+
+        if (word_setter.length > 1) {
+          second_letter = word_setter[word_setter.length - 1];
+          last_letter = word_setter[1];
+          word_setter[1] = String(second_letter);
+          word_setter[word_setter.length - 1] = String(last_letter);
+          word_setter = word_setter.join("");
+          new_phrase.push(word_setter);
+        } else {
+          word_setter = word_setter.join("");
+          new_phrase.push(word_setter);
+        }
+      }
+    }
+    new_phrase = new_phrase.join(" ");
+    document.getElementById("input").value = new_phrase;
+  }
 
   return (
     <>
@@ -106,7 +172,7 @@ const GelbckeEncryption = () => {
               id="buttonTest"
               className={Styles.styledButtonDecrypt}
               style={{ marginLeft: "5px" }}
-              onClick={() => convertToEncryption()}
+              onClick={() => convertToText()}
             >
               Decrypt
             </button>
